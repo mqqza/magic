702ce8e18f8ae4b936a42112e1801ab3435c376e
SoulReaverQ <michail.belyaevq@gmail.com>
2015-04-11 12:06:28 +0300

    Second commit

--- app/src/main/java/com/soulreaverq/magic/MainActivity.java
+++ app/src/main/java/com/soulreaverq/magic/MainActivity.java
@@ -1,6 +1,7 @@
 package com.soulreaverq.magic;
 
 import android.app.Activity;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -13,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends ActionBarActivity
