5a6cf6f6c49cee78f0dfe7806e088023d74df0e6
Misha <michail.belyaevq@gmail.com>
2015-04-14 16:38:28 +0300

    fixed shadow of toolbar, added gridview

--- app/src/main/java/com/soulreaverq/magic/MyFragment1.java
+++ app/src/main/java/com/soulreaverq/magic/MyFragment1.java
@@ -1,12 +1,8 @@
 package com.soulreaverq.magic;
 
 import android.content.ClipData;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,17 +14,22 @@ import java.util.ArrayList;
 public class MyFragment1 extends Fragment {
 
     GridView gridView;
-    ArrayList<ClipData.Item> gridArray = new ArrayList<ClipData.Item>();
-    GridViewAdapter gridAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_one, container, false);
-        Bitmap image = BitmapFactory.decodeResource(this.getResources(),R.drawable.example_image);
-        gridArray.add(new Item(image));
-
+        int[] images = {
+                R.drawable.untitled, R.drawable.untitled, R.drawable.untitled,
+                R.drawable.untitled, R.drawable.untitled, R.drawable.untitled,
+                R.drawable.untitled, R.drawable.untitled, R.drawable.untitled,
+                R.drawable.untitled, R.drawable.untitled, R.drawable.untitled,
+                R.drawable.untitled, R.drawable.untitled, R.drawable.untitled,
+        };
+        // gridArray.add(new Item(image));
+        gridView = (GridView) view.findViewById(R.id.grid_view);
+        gridView.setAdapter(new GridViewAdapter(getActivity(), images));
         return view;
     }
 }
