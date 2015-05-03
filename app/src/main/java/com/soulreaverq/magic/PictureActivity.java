11947e6e8b0dea2e479e584429b1063c9802a390
Misha <michail.belyaevq@gmail.com>
2015-05-03 21:23:07 +0300

    * ADD fade toolbar for picture activity

--- app/src/main/java/com/soulreaverq/magic/PictureActivity.java
+++ app/src/main/java/com/soulreaverq/magic/PictureActivity.java
@@ -1,46 +1,75 @@
 package com.soulreaverq.magic;
 
-import android.app.Activity;
-import android.app.FragmentManager;
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
+import com.github.ksoichiro.android.observablescrollview.ScrollUtils;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
 
-public class PictureActivity extends ActionBarActivity {
+public class PictureActivity extends AppCompatActivity implements ObservableScrollViewCallbacks {
 
-    private static final String TAG = "CCC";
     private String mTitle;
     private String mImage;
+    private Toolbar mToolbarView;
+    private ObservableScrollView mScrollView;
+    private int mParallaxImageHeight;
+    SquareImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
 
-        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mToolbar);
-        mToolbar.setSubtitle("Picture");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mTitle = getIntent().getStringExtra("title");
-        mImage = getIntent().getStringExtra("image");
+        mToolbarView = (Toolbar) findViewById(R.id.toolbar);
+        mToolbarView.setBackgroundColor(ScrollUtils.getColorWithAlpha(0, getResources().getColor(R.color.colorPrimary)));
+        setSupportActionBar(mToolbarView);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        mScrollView = (ObservableScrollView) findViewById(R.id.scroll);
+        mScrollView.setScrollViewCallbacks(this);
 
         TextView textView = (TextView) findViewById(R.id.title);
-        SquareImageView imageView = (SquareImageView) findViewById(R.id.image);
-
+        mTitle = getIntent().getStringExtra("title");
         textView.setText("LIKES: " + mTitle);
+
+        imageView = (SquareImageView) findViewById(R.id.image);
+        mImage = getIntent().getStringExtra("image");
         ImageLoader.getInstance().displayImage(mImage, imageView);
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        mParallaxImageHeight = imageView.getHeight();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        onScrollChanged(mScrollView.getCurrentScrollY(), false, false);
+    }
+
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+        int baseColor = getResources().getColor(R.color.colorPrimary);
+        float alpha = 1 - (float) Math.max(0, mParallaxImageHeight - scrollY) / mParallaxImageHeight;
+        mToolbarView.setBackgroundColor(ScrollUtils.getColorWithAlpha(alpha, baseColor));
+        // ViewHelper.setTranslationY(mImageView, scrollY / 2);
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+    }
+
 }
